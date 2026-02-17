9fddbb11a6cf7b3d2e1269a07b804b5dc8a57c13
Deannv <afilfriend@gmail.com>
2026-02-17 14:48:54 +0800

    Initial Commit

---
+++ SecureJournalistAppTests/SecureJournalistAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SecureJournalistAppTests.swift
+//  SecureJournalistAppTests
+//
+//  Created by Kemas Deanova on 17/02/26.
+//
+
+import Testing
+@testable import SecureJournalistApp
+
+struct SecureJournalistAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
